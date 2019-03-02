ff7eeee663cfacd8868d18904c829a4c0b35460b
Katie Moosman <moosman.katie@gmail.com>
2019-03-02 00:54:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,23 +31,40 @@ class App < Sinatra::Base
 
   end
 
+  # get '/:operation/:number1/:number2' do
+  #   @operation = params[:operation]
+  #   @num1 = params[:number1].to_i
+  #   @num2 = params[:number2].to_i
+  #
+  #   case operation
+	# 	when 'add'
+	# 		"#{@num1 + @num2}"
+	# 	when 'subtract'
+	# 		(@num1 - @num2).to_s
+	# 	when 'multiply'
+	# 		(@num1 * @num2).to_s
+	# 	when 'divide'
+	# 		(@num1 / @num2).to_s
+	# 	end
+  #
+  #
+  # end
+
   get '/:operation/:number1/:number2' do
-    @operation = params[:operation]
-    @num1 = params[:number1].to_i
-    @num2 = params[:number2].to_i
+		operation = params[:operation]
+		number1 = params[:number1].to_i
+		number2 = params[:number2].to_i
 
-    case operation
+		case operation
 		when 'add'
-			"#{@num1 + @num2}"
+			(number1 + number2).to_s
 		when 'subtract'
-			(@num1 - @num2).to_s
+			(number1 - number2).to_s
 		when 'multiply'
-			(@num1 * @num2).to_s
+			(number1 * number2).to_s
 		when 'divide'
-			(@num1 / @num2).to_s
+			(number1 / number2).to_s
 		end
-
-
-  end
+	end
 
 end
