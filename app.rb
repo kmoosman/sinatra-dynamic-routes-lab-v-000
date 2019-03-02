c95feb18fdabfdd6f833f25994a5a52f41ea957e
Katie Moosman <moosman.katie@gmail.com>
2019-03-02 00:24:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,7 +13,7 @@ class App < Sinatra::Base
     "#{@num * @num}"
   end
 
-  get "/:number/:phrase" do
+  get "say/:number/:phrase" do
     @num = params[:number].to_i
     @phrase = params[:phrase]
 
